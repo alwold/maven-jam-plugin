cce8676e7194297a3f92a8be1617e1c943b9f4eb
Al Wold <alwold@gmail.com>
2010-07-15 10:55:41 -0700

    - pull out common deploy stuff into an abstract class - recursively create any missing dirs

---
+++ src/main/java/edu/asu/wit/maven/AbstractDeployMojo.java
@@ -0,0 +1,109 @@
+package edu.asu.wit.maven;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.DirectoryScanner;
+
+/**
+ *
+ * @author alwold
+ */
+public abstract class AbstractDeployMojo extends AbstractMojo {
+    /**
+     * @parameter
+     */
+    private String appDir;
+
+    /**
+     * @parameter expression="${project.build.directory}"
+     * @required
+     */
+    private String outputDirectory;
+
+    /**
+     * The name of the generated WAR.
+     *
+     * @parameter expression="${project.build.finalName}"
+     * @required
+     */
+    private String warName;
+
+    /**
+     * @parameter expression="${project.compileSourceRoots}"
+     * @required
+     */
+    private List compileSourceRoots;
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("Deploying to "+appDir);
+		// copy all of the generated WAR stuff, except WEB-INF/classes
+		DirectoryScanner scanner = new DirectoryScanner();
+		File webappBase = new File(outputDirectory, warName);
+		scanner.setBasedir(webappBase);
+		// exclude jetty-env.xml because it might include passwords and it isn't needed
+		scanner.setExcludes(new String[] { "WEB-INF/classes/**", "WEB-INF/jetty-env.xml", "WEB-INF" });
+		scanner.scan();
+		String[] files = scanner.getIncludedFiles();
+		boolean foundDevAppProperties = false;
+		for (int i = 0; i < files.length; i++) {
+			String name = files[i];
+			String destination = appDir + "/" + name;
+
+			File source = new File(webappBase, name);
+			File destinationFile = new File(destination);
+
+			performCopy(source.getAbsolutePath(), destinationFile);
+		}
+		// copy everything from WEB-INF/classes that isn't a class file to WEB-INF/src
+		scanner = new DirectoryScanner();
+		File base = new File(webappBase, "WEB-INF");
+		base = new File(base, "classes");
+		scanner.setBasedir(base);
+		scanner.setExcludes(new String[]{"**/*.class"});
+		scanner.scan();
+		files = scanner.getIncludedFiles();
+		for (int i = 0; i < files.length; i++) {
+			String name = files[i];
+			String destination = appDir + "/WEB-INF/src/" + name;
+
+			getLog().info(name);
+			if (foundDevAppProperties && name.equals("application.properties")) {
+				// if we already found a specific application.properties for dev, skip this
+				continue;
+			} else if (name.equals("application.properties.dev")) {
+				destination = appDir + "/WEB-INF/src/application.properties";
+				foundDevAppProperties = true;
+			}
+
+			File source = new File(base, name);
+			File destinationFile = new File(destination);
+
+			performCopy(source.getAbsolutePath(), destinationFile);
+		}
+		// now get the source code
+		for (Iterator i = compileSourceRoots.iterator(); i.hasNext(); ) {
+			String root = (String)i.next();
+			scanner = new DirectoryScanner();
+			scanner.setBasedir(root);
+			scanner.setIncludes(new String[]{"**/*.java"});
+			scanner.scan();
+			files = scanner.getIncludedFiles();
+			for (int j = 0; j < files.length; j++) {
+				String name = files[j];
+				String destination = appDir + "/WEB-INF/src/" + name;
+
+				File source = new File(root, name);
+				File destinationFile = new File(destination);
+
+				performCopy(source.getAbsolutePath(), destinationFile);
+			}
+		}
+	}
+
+	public abstract void performCopy(String source, File destinationFile) throws MojoExecutionException;
+
+}
--- src/main/java/edu/asu/wit/maven/SshDeploy.java
+++ src/main/java/edu/asu/wit/maven/SshDeploy.java
@@ -11,23 +11,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
-import java.util.List;
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.codehaus.plexus.util.DirectoryScanner;
 
 /**
  * @goal sshdeploy
  * @execute phase="install"
  * @author alwold
  */
-public class SshDeploy extends AbstractMojo {
-    /**
-     * @parameter
-     */
-    private String appDir;
+public class SshDeploy extends AbstractDeployMojo {
 
 	/**
 	 * @parameter
@@ -35,26 +27,10 @@ public class SshDeploy extends AbstractMojo {
 	 */
 	private String sshHost;
 
-    /**
-     * @parameter expression="${project.build.directory}"
-     * @required
-     */
-    private String outputDirectory;
+	private ChannelSftp sftp;
 
-    /**
-     * The name of the generated WAR.
-     *
-     * @parameter expression="${project.build.finalName}"
-     * @required
-     */
-    private String warName;
 
-    /**
-     * @parameter expression="${project.compileSourceRoots}"
-     * @required
-     */
-    private List compileSourceRoots;
-
+	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		try {
 			getLog().info("Please enter your username:");
@@ -90,118 +66,40 @@ public class SshDeploy extends AbstractMojo {
 				}
 			});
 			session.connect();
-			ChannelSftp sftp = (ChannelSftp) session.openChannel("sftp");
+			sftp = (ChannelSftp) session.openChannel("sftp");
 			sftp.connect();
-			getLog().info("Deploying to "+appDir);
-			// copy all of the generated WAR stuff, except WEB-INF/classes
-			DirectoryScanner scanner = new DirectoryScanner();
-			File webappBase = new File(outputDirectory, warName);
-			scanner.setBasedir(webappBase);
-			// exclude jetty-env.xml because it might include passwords and it isn't needed
-			scanner.setExcludes(new String[] { "WEB-INF/classes/**", "WEB-INF/jetty-env.xml", "WEB-INF" });
-			scanner.scan();
-			String[] files = scanner.getIncludedFiles();
-			boolean foundDevAppProperties = false;
-			for (int i = 0; i < files.length; i++) {
-				String name = files[i];
-				String destination = appDir + "/" + name;
-
-				File source = new File(webappBase, name);
-				File destinationFile = new File(destination);
-
-				try {
-					SftpATTRS attrs = null;
-					try {
-						attrs = sftp.stat(destinationFile.getParent());
-					} catch (SftpException e) {}
-					if (attrs == null) {
-						getLog().info("creating "+destinationFile.getParent());
-						sftp.mkdir(destinationFile.getParent());
-					}
-
-					getLog().info("copying "+source.getAbsolutePath()+" ->");
-					getLog().info(destinationFile.getAbsolutePath());
-					sftp.put(source.getAbsolutePath(), destination);
-				} catch (SftpException e) {
-					throw new MojoExecutionException("Error copying webapp file " + source, e);
-				}
-			}
-			// copy everything from WEB-INF/classes that isn't a class file to WEB-INF/src
-			scanner = new DirectoryScanner();
-			File base = new File(webappBase, "WEB-INF");
-			base = new File(base, "classes");
-			scanner.setBasedir(base);
-			scanner.setExcludes(new String[]{"**/*.class"});
-			scanner.scan();
-			files = scanner.getIncludedFiles();
-			for (int i = 0; i < files.length; i++) {
-				String name = files[i];
-				String destination = appDir + "/WEB-INF/src/" + name;
-
-				getLog().info(name);
-				if (foundDevAppProperties && name.equals("application.properties")) {
-					// if we already found a specific application.properties for dev, skip this
-					continue;
-				} else if (name.equals("application.properties.dev")) {
-					destination = appDir + "/WEB-INF/src/application.properties";
-					foundDevAppProperties = true;
-				}
-
-				File source = new File(base, name);
-				File destinationFile = new File(destination);
-
-				try {
-					SftpATTRS attrs = null;
-					try {
-						attrs = sftp.stat(destinationFile.getParent());
-					} catch (SftpException e) {}
-					if (attrs == null) {
-						getLog().info("creating "+destinationFile.getParent());
-						sftp.mkdir(destinationFile.getParent());
-					}
-
-					getLog().info("copying "+source.getAbsolutePath()+" to "+destinationFile.getAbsolutePath());
-					sftp.put(source.getAbsolutePath(), destination);
-				} catch (SftpException e) {
-					throw new MojoExecutionException("Error copying webapp file " + source, e);
-				}
-			}
-			// now get the source code
-			for (Iterator i = compileSourceRoots.iterator(); i.hasNext(); ) {
-				String root = (String)i.next();
-				scanner = new DirectoryScanner();
-				scanner.setBasedir(root);
-				scanner.setIncludes(new String[]{"**/*.java"});
-				scanner.scan();
-				files = scanner.getIncludedFiles();
-				for (int j = 0; j < files.length; j++) {
-					String name = files[j];
-					String destination = appDir + "/WEB-INF/src/" + name;
-
-					File source = new File(root, name);
-					File destinationFile = new File(destination);
-
-					try {
-						SftpATTRS attrs = null;
-						try {
-							attrs = sftp.stat(destinationFile.getParent());
-						} catch (SftpException e) {}
-						if (attrs == null) {
-							sftp.mkdir(destinationFile.getParent());
-						}
-
-						getLog().info("copying "+source.getAbsolutePath()+" ->");
-						getLog().info(destinationFile.getAbsolutePath());
-						sftp.put(source.getAbsolutePath(), destination);
-					} catch (SftpException e) {
-						throw new MojoExecutionException("Error copying webapp file " + source, e);
-					}
-				}
-			}
+			super.execute();
 		} catch (JSchException e) {
 			throw new MojoExecutionException("Unable to start SSH session", e);
 		} catch (IOException e) {
 			throw new MojoExecutionException("Unable to read credentials", e);
+		}
+	}
+
+	@Override
+	public void performCopy(String source, File destinationFile) throws MojoExecutionException {
+		try {
+			ensureDirectoryExists(destinationFile.getParentFile());
+
+			getLog().info("copying "+source+" ->");
+			getLog().info(destinationFile.getAbsolutePath());
+			sftp.put(source, destinationFile.getAbsolutePath());
+		} catch (SftpException e) {
+			throw new MojoExecutionException("Error copying webapp file " + source, e);
+		}
+	}
+
+	private void ensureDirectoryExists(File dir) throws SftpException {
+		SftpATTRS attrs = null;
+		try {
+			attrs = sftp.stat(dir.getAbsolutePath());
+		} catch (SftpException e) {}
+		if (attrs == null) {
+			if (dir.getParentFile() != null) {
+				ensureDirectoryExists(dir.getParentFile());
+			}
+			getLog().info("creating "+dir.getAbsolutePath());
+			sftp.mkdir(dir.getAbsolutePath());
 		}
 	}
 
